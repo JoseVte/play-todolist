b3517f418ad475063e85f45a69f90b1ab0020ac6
Josrom <jvortsromero@gmail.com>
2014-09-19 14:39:26 +0200

    Caracteristica nueva: Creacion de nueva tarea (1 de 2)
    - Implementacion del codigo para devolver la descripcion como Json o un error

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -30,24 +30,25 @@ object Application extends Controller {
 		Ok(views.html.index(Task.all(), taskForm))
 	}
 
-  def readTask(id: Long) = Action {
-    try{
-      val json = Json.toJson(Task.read(id))
-      Ok(json)
+   def readTask(id: Long) = Action {
+      try{
+         val json = Json.toJson(Task.read(id))
+         Ok(json)
       } catch {
-        case _ => NotFound("Error 404: La tarea con el identificador "+id+" no existe")
+         case _ => NotFound("Error 404: La tarea con el identificador "+id+" no existe")
       }
-  }
+   }
 
 	def newTask = Action { implicit request =>
-		taskForm.bindFromRequest.fold(
-			errors => BadRequest(views.html.index(Task.all(),errors)),
-			label => {
-				Task.create(label)
-				Redirect(routes.Application.tasks)
-		}
-	)
-	}
+      taskForm.bindFromRequest.fold(
+         errors => BadRequest("Error 500: No se ha podido crear la nueva tarea"),
+         label => {
+            Task.create(label)
+            val json = Json.toJson(label)
+            Created(json)
+         }
+      )
+   }
 
 	def deleteTask(id: Long) = Action {
 		Task.delete(id)

651d8f9f5bc283332c9c7c7485e18cce8b5224ba
Josrom <jvortsromero@gmail.com>
2014-10-07 23:27:04 +0200

    Refactorizacion del parse e indentar el codigo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,116 +17,117 @@ import models.Task
 
 object Application extends Controller {
 
-  val parseDate: String = "^([0-9]{1,2}-[0-9]{1,2}-[0-9]{4})$"
+   val parseDate: String = "^([0-9]{1,2}-[0-9]{1,2}-[0-9]{4})$"
+   val formatoParse: String = "dd-MM-yyyy"
 
-	val taskForm = Form(
-    mapping(
-      "id" -> ignored(0L),
-      "label" -> nonEmptyText,
-      "fechaFin" -> optional(date("dd/mm/yyyy"))
-    )(Task.apply)(Task.unapply)
-	)
+   val taskForm = Form(
+      mapping(
+         "id" -> ignored(0L),
+         "label" -> nonEmptyText,
+         "fechaFin" -> optional(date("dd/mm/yyyy"))
+      )(Task.apply)(Task.unapply)
+   )
 
-  implicit val taskWrites = new Writes[Task] {
-    def writes(task: Task) = Json.obj(
-      "id" -> task.id,
-      "label" -> task.label,
-      "fechaFin" -> task.fechaFin)
-  }
+   implicit val taskWrites = new Writes[Task] {
+      def writes(task: Task) = Json.obj(
+         "id" -> task.id,
+         "label" -> task.label,
+         "fechaFin" -> task.fechaFin)
+   }
 
-  def errores(mensajeError: String) : String = {
-    var error: String = "<html><head><title>- ooops! -</title><style>body { background:#0000aa;color:#ffffff;font-family:courier;font-size:12pt;text-align:center;margin:100px;}blink {color:yellow;}.neg {background:#fff;color:#0000aa;padding:2px 8px;font-weight:bold;}p {margin:30px 100px;text-align:left;font-size: 20;}a,a:hover {color:inherit;font:inherit;}</style></head><body><h1><span class=\"neg\">"
-    error+=mensajeError
-    error+="</span></h1><p><br>Usted puede esperar y ver si vuelve a estar disponible, o puede reiniciar su PC.</p><p>* Envienos un e-mail para notificar esto e intentelo mas tarde.<br />* Pulse CTRL+ALT+SUPR para reiniciar su PC. Usted perder&aacute; toda la informaci&oacute;n no guardada en cualquier programa que este ejecutando.</p>Pulse cualquier link para continuar<blink>_</blink><div class=\"menu\"><a href=\"\">Recargar</a> | <a href=\"http://www.google.es\">Google</a> |</div></body></html>";
-    return error;
-  }
+   def errores(mensajeError: String) : String = {
+      var error: String = "<html><head><title>- ooops! -</title><style>body { background:#0000aa;color:#ffffff;font-family:courier;font-size:12pt;text-align:center;margin:100px;}blink {color:yellow;}.neg {background:#fff;color:#0000aa;padding:2px 8px;font-weight:bold;}p {margin:30px 100px;text-align:left;font-size: 20;}a,a:hover {color:inherit;font:inherit;}</style></head><body><h1><span class=\"neg\">"
+      error+=mensajeError
+      error+="</span></h1><p><br>Usted puede esperar y ver si vuelve a estar disponible, o puede reiniciar su PC.</p><p>* Envienos un e-mail para notificar esto e intentelo mas tarde.<br />* Pulse CTRL+ALT+SUPR para reiniciar su PC. Usted perder&aacute; toda la informaci&oacute;n no guardada en cualquier programa que este ejecutando.</p>Pulse cualquier link para continuar<blink>_</blink><div class=\"menu\"><a href=\"\">Recargar</a> | <a href=\"http://www.google.es\">Google</a> |</div></body></html>";
+      return error;
+   }
 
-	def index = Action {
-  	Redirect(routes.Application.tasks("anonimo"))
-	}
+   def index = Action {
+      Redirect(routes.Application.tasks("anonimo"))
+   }
 
-	def tasks(usuario: String) = Action {
-    if(Task.comprobarUsuario(usuario)){
-      val json = Json.toJson(Map(usuario -> Json.toJson(Task.all(usuario))))
-		  Ok(json)
-    } else {
-      NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-    }
-	}
-
-  def readTask(usuario: String, id: Long) = Action {
-    if(Task.comprobarUsuario(usuario)){
-      Task.read(usuario,id) match {
-        case Some(task) => 
-          val json = Json.toJson(task)
-          Ok(json)
-        case None => NotFound(errores("Error 404: La tarea con el identificador "+id+" no existe en el usuario "+usuario)).as("text/html")
-      }
-    } else {
-      NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-    }
-  }
-
-  def tasksFinalizadas(usuario: String, fecha: String) = Action {
-    if(Task.comprobarUsuario(usuario)){
-      val formatoURI = new SimpleDateFormat("dd-MM-yyyy")
-      var fechaParse = new Date()
-      if(fecha != null && fecha.matches(parseDate)){
-        fechaParse = formatoURI.parse(fecha)
-        val json = Json.toJson(Task.all(usuario,fechaParse))
-        Ok(json)
-      }else if(fecha == null){
-        fechaParse = Calendar.getInstance().getTime()
-        val json = Json.toJson(Task.all(usuario,fechaParse))
-        Ok(json)
+   def tasks(usuario: String) = Action {
+      if(Task.comprobarUsuario(usuario)){
+         val json = Json.toJson(Map(usuario -> Json.toJson(Task.all(usuario))))
+         Ok(json)
       } else {
-        NotFound(errores("Error 404: La fecha ("+fecha+") no esta en el formato correcto")).as("text/html")
+         NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
       }
-    } else {
-      NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-    }
-  }
+   }
 
-	def newTask(usuario: String) = Action { implicit request =>
-    taskForm.bindFromRequest.fold(
-      errors => BadRequest,
-      task => {
-        if(Task.comprobarUsuario(usuario)){
-          val id = Task.create(task.label,usuario,task.fechaFin)
-          val json = Json.toJson(Map(usuario -> Json.toJson(new Task(id,task.label,task.fechaFin))))
-          Created(json)
-        } else {
-          NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-        }
-      }
-    )
-  }
-
-	def deleteTask(usuario: String, id: Long) = Action {
-		if(Task.comprobarUsuario(usuario)){
-      val resultado : Int = Task.delete(usuario,id)
-  		if(resultado == 1){
-        Ok("Tarea "+id+" del usuario "+usuario+" borrada correctamente")
+   def readTask(usuario: String, id: Long) = Action {
+      if(Task.comprobarUsuario(usuario)){
+         Task.read(usuario,id) match {
+            case Some(task) => 
+               val json = Json.toJson(task)
+               Ok(json)
+            case None => NotFound(errores("Error 404: La tarea con el identificador "+id+" no existe en el usuario "+usuario)).as("text/html")
+         }
       } else {
-        NotFound(errores("Error 404: La tarea con el identificador "+id+" no existe para el usuario "+usuario)).as("text/html")
+         NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
       }
-    } else {
-      NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-    }
-	}
+   }
 
-  def deleteTaskDate(usuario: String, fecha: String) = Action {
-    if(Task.comprobarUsuario(usuario)){
-      val formatoURI = new SimpleDateFormat("dd-MM-yyyy")
-      if(fecha != null && fecha.matches(parseDate)){
-        val fechaParse : Date = formatoURI.parse(fecha)
-        val numRows : Int = Task.deleteDate(usuario,fechaParse)
-        Ok("Se han borrado "+numRows+" de tareas del usuario "+usuario+" hasta la fecha "+fecha)
+   def tasksFinalizadas(usuario: String, fecha: String) = Action {
+      if(Task.comprobarUsuario(usuario)){
+         val formatoURI = new SimpleDateFormat(formatoParse)
+         var fechaParse = new Date()
+         if(fecha != null && fecha.matches(parseDate)){
+            fechaParse = formatoURI.parse(fecha)
+            val json = Json.toJson(Task.all(usuario,fechaParse))
+            Ok(json)
+         }else if(fecha == null){
+            fechaParse = Calendar.getInstance().getTime()
+            val json = Json.toJson(Task.all(usuario,fechaParse))
+            Ok(json)
+         } else {
+            NotFound(errores("Error 400: La fecha ("+fecha+") no esta en el formato correcto")).as("text/html")
+         }
       } else {
-        NotFound(errores("Error 404: La fecha ("+fecha+") no esta en el formato correcto")).as("text/html")
+         NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
       }
-    } else {
-      NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
-    }
-  }
+   }
+
+   def newTask(usuario: String) = Action { implicit request =>
+      taskForm.bindFromRequest.fold(
+         errors => BadRequest,
+         task => {
+            if(Task.comprobarUsuario(usuario)){
+               val id = Task.create(task.label,usuario,task.fechaFin)
+               val json = Json.toJson(Map(usuario -> Json.toJson(new Task(id,task.label,task.fechaFin))))
+               Created(json)
+            } else {
+               NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
+            }
+         }
+      )
+   }
+
+   def deleteTask(usuario: String, id: Long) = Action {
+      if(Task.comprobarUsuario(usuario)){
+         val resultado : Int = Task.delete(usuario,id)
+         if(resultado == 1){
+            Ok("Tarea "+id+" del usuario "+usuario+" borrada correctamente")
+         } else {
+            NotFound(errores("Error 404: La tarea con el identificador "+id+" no existe para el usuario "+usuario)).as("text/html")
+         }
+      } else {
+         NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
+      }
+   }
+
+   def deleteTaskDate(usuario: String, fecha: String) = Action {
+      if(Task.comprobarUsuario(usuario)){
+         val formatoURI = new SimpleDateFormat(formatoParse)
+         if(fecha != null && fecha.matches(parseDate)){
+            val fechaParse : Date = formatoURI.parse(fecha)
+            val numRows : Int = Task.deleteDate(usuario,fechaParse)
+            Ok("Se han borrado "+numRows+" de tareas del usuario "+usuario+" hasta la fecha "+fecha)
+         } else {
+            NotFound(errores("Error 400: La fecha ("+fecha+") no esta en el formato correcto")).as("text/html")
+         }
+      } else {
+         NotFound(errores("Error 404: El usuario "+usuario+" no existe")).as("text/html")
+      }
+   }
 }
--- app/models/Task.scala
+++ app/models/Task.scala
@@ -11,70 +11,70 @@ import java.util.Date
 case class Task(id: Long, label: String, fechaFin: Option[Date])
 
 object Task {
-	val task = {
-		get[Long]("id") ~
-		get[String]("label") ~
-		get[Option[Date]]("fechaFin") map{
-			case id~label~fechaFin => Task(id,label,fechaFin)
-		}
-	}
+   val task = {
+      get[Long]("id") ~
+      get[String]("label") ~
+      get[Option[Date]]("fechaFin") map{
+         case id~label~fechaFin => Task(id,label,fechaFin)
+      }
+   }
 
-	val user = {
-		int("id") ~ str("nombre") map{
-			case id~nombre => (id,nombre)
-		}
-	}
+   val user = {
+      int("id") ~ str("nombre") map{
+         case id~nombre => (id,nombre)
+      }
+   }
 
-	def comprobarUsuario(usuario: String): Boolean = {
-    	val aux = DB.withConnection{
-    		implicit c => SQL("select * from usuarios where nombre = {usuario}").on("usuario" -> usuario).as(user.*)
-    	}
-    	return !aux.isEmpty
-	}
+   def comprobarUsuario(usuario: String): Boolean = {
+      val aux = DB.withConnection{
+         implicit c => SQL("select * from usuarios where nombre = {usuario}").on("usuario" -> usuario).as(user.*)
+      }
+      return !aux.isEmpty
+   }
 
-	def all(usuario: String): List[Task] = DB.withConnection{
-		implicit c => SQL("select * from task where usuario = {usuario}").on("usuario" -> usuario).as(task *)
-	}
+   def all(usuario: String): List[Task] = DB.withConnection{
+      implicit c => SQL("select * from task where usuario = {usuario}").on("usuario" -> usuario).as(task *)
+   }
 
-	def all(usuario: String, fecha: Date): List[Task] = DB.withConnection{
-		implicit c => SQL("select * from task where usuario = {usuario} and fechaFin <= {fecha}").on("usuario" -> usuario,"fecha" -> fecha).as(task *)
-	}
+   def all(usuario: String, fecha: Date): List[Task] = DB.withConnection{
+      implicit c => SQL("select * from task where usuario = {usuario} and fechaFin <= {fecha}").on("usuario" -> usuario,"fecha" -> fecha).as(task *)
+   }
 
-	def read(usuario: String, id: Long): Option[Task] = DB.withConnection{ 
-		implicit c =>
-			SQL("select * from task where id = {id} and usuario = {usuario}")
-			.on("id" -> id, "usuario" -> usuario).as(task.singleOpt)
-	}
+   def read(usuario: String, id: Long): Option[Task] = DB.withConnection{ 
+      implicit c =>
+         SQL("select * from task where id = {id} and usuario = {usuario}")
+         .on("id" -> id, "usuario" -> usuario).as(task.singleOpt)
+   }
 
-	def create(label: String, usuario: String,fechaFin: Option[Date]) : Long = {
-		var idNuevo: Long = 0
-		var aux: Date = new Date
-		if(!fechaFin.isEmpty){
-			aux = fechaFin.get
-		}
-		DB.withConnection{
-			implicit c => 
-				idNuevo = SQL("insert into task(label,usuario,fechaFin) values ({label},{usuario},{fechaFin})")
-				.on("label" -> label, "usuario" -> usuario,"fechaFin" -> aux).executeInsert().get
-		}
-		return idNuevo
-	}
+   def create(label: String, usuario: String,fechaFin: Option[Date]) : Long = {
+      var idNuevo: Long = 0
+      var aux: Date = new Date
+      if(!fechaFin.isEmpty){
+         aux = fechaFin.get
+      }
+      DB.withConnection{
+         implicit c => 
+            idNuevo = SQL("insert into task(label,usuario,fechaFin) values ({label},{usuario},{fechaFin})")
+            .on("label" -> label, "usuario" -> usuario,"fechaFin" -> aux).executeInsert().get
+      }
+      return idNuevo
+   }
 
-	def delete(usuario: String, id: Long) : Int = {
-		var numRows = 0
-		DB.withConnection{
-			implicit c => 
-				numRows = SQL("delete from task where id = {id} and usuario = {usuario}").on("id" -> id,"usuario" -> usuario).executeUpdate()
-		}
-		return numRows
-	}
+   def delete(usuario: String, id: Long) : Int = {
+      var numRows = 0
+      DB.withConnection{
+         implicit c => 
+            numRows = SQL("delete from task where id = {id} and usuario = {usuario}").on("id" -> id,"usuario" -> usuario).executeUpdate()
+      }
+      return numRows
+   }
 
-	def deleteDate(usuario: String, fecha: Date) : Int = {
-		var numRows = 0
-		DB.withConnection{
-			implicit c =>
-				numRows = SQL("delete from task where usuario = {usuario} and fechaFin < {fecha}").on("usuario" -> usuario,"fecha" -> fecha).executeUpdate()
-		}
-		return numRows
-	}
+   def deleteDate(usuario: String, fecha: Date) : Int = {
+      var numRows = 0
+      DB.withConnection{
+         implicit c =>
+            numRows = SQL("delete from task where usuario = {usuario} and fechaFin < {fecha}").on("usuario" -> usuario,"fecha" -> fecha).executeUpdate()
+      }
+      return numRows
+   }
 }

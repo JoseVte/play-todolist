e3579baee4b2ef29e4eef7d31e1f79d83a38c752
Josrom <jvortsromero@gmail.com>
2014-10-18 17:29:43 +0200

    Nueva test para controlador: una tarea
    - Tambien se controla si la tarea no existe

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -33,5 +33,21 @@ class ApplicationSpec extends Specification {
                 contentAsString(pag) must contain ("anonimo") 
             }
         }
+
+        "una tarea concreta" in {
+            running(FakeApplication()) {
+                val Some(pag) = route(FakeRequest(GET,"/tasks/1"))
+
+                status(pag) must equalTo(OK)
+                contentType(pag) must beSome.which(_ == "application/json")
+                contentAsString(pag) must contain ("\"id\":1")
+
+                // Comprobamos que una tarea no exista
+                val Some(error) = route(FakeRequest(GET,"/tasks/0"))
+                status(error) must equalTo(NOT_FOUND)
+                contentType(error) must beSome.which(_ == "text/html")
+                contentAsString(error) must contain ("404")
+            }
+        }
     }
 }
